91222b31426ec1e561be39592fb8b87f9261bcde
Jerry James <loganjerry@gmail.com>
2018-11-01 20:46:14 -0600

    Replace ancient FSF address with the canonical URL.

--- lib/braiding.h
+++ lib/braiding.h
@@ -14,8 +14,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Braiding; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+    along with Braiding.  If not, see <https://www.gnu.org/licenses/>.
 */
 /*
     braiding.h,  v 1.0.   04/10/2004
