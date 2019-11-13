2c3dd7e1da4fed1f27f3c804c15e868c0f5bec6e
Jean-Luc Thiffeault <jeanluc@mailaps.org>
2019-11-13 08:50:27 -0600

    Get rid of register keyword (not C++17 compliant).

--- lib/cbraid_implementation.h
+++ lib/cbraid_implementation.h
@@ -428,7 +428,7 @@ inline Factor<P>::~Factor()
 template<class P>
 inline Factor<P>& Factor<P>::Delta(sint32 k)
 {
-    for(register sint16 i = 1; i <= Index(); ++i)
+    for(sint16 i = 1; i <= Index(); ++i)
         At(i) = Pres.DeltaTable(i, k);
     return *this;
 }
@@ -519,7 +519,7 @@ inline Factor<P>& Factor<P>::Assign(const Factor<P>& f)
 #endif
 
     if (&f != this) {
-        for(register sint16 i = 1; i <= Index(); ++i) {
+        for(sint16 i = 1; i <= Index(); ++i) {
             At(i) = f[i];
         }
     }
@@ -545,7 +545,7 @@ inline bool Factor<P>::Compare(const Factor<P>& f) const
     }
 #endif
 
-    for(register sint16 i = 1; i <= Index(); ++i) {
+    for(sint16 i = 1; i <= Index(); ++i) {
         if (At(i) != f[i])
             return false;
     }
@@ -570,7 +570,7 @@ inline bool Factor<P>::operator!=(const Factor& f) const
 template<class P>
 inline bool Factor<P>::CompareWithDelta(sint32 k) const
 {
-    for(register sint16 i = 1; i <= Index(); ++i) {
+    for(sint16 i = 1; i <= Index(); ++i) {
         if (At(i) != Pres.DeltaTable(i, k))
             return false;
     }
@@ -596,7 +596,7 @@ inline Factor<P> Factor<P>::Composition(
     }
 #endif
     Factor f(Index());
-    for(register sint16 i = 1; i <= Index(); ++i)
+    for(sint16 i = 1; i <= Index(); ++i)
         f[i] = a[At(i)];
     return f;
 }
@@ -612,7 +612,7 @@ inline Factor<P>& Factor<P>::AssignComposition(
         exit(1);
     }
 #endif
-    for(register sint16 i = 1; i <= Index(); ++i)
+    for(sint16 i = 1; i <= Index(); ++i)
         At(i) = a[At(i)];
     return *this;
 }
@@ -636,7 +636,7 @@ template<class P>
 inline Factor<P> Factor<P>::Inverse() const
 {
     Factor f(Index());
-    for(register sint16 i = 1; i <= Index(); ++i)
+    for(sint16 i = 1; i <= Index(); ++i)
         f[At(i)] = i;
     return f;
 }
@@ -660,7 +660,7 @@ template<class P>
 inline Factor<P> Factor<P>::Flip(sint32 k) const
 {
     Factor f(Index());
-    for(register sint16 i = 1; i <= Index(); ++i)
+    for(sint16 i = 1; i <= Index(); ++i)
         f[i] = Pres.DeltaTable(At(Pres.DeltaTable(i, -k)), k);
     return f;
 }
